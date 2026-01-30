734f7da634f42d89a238409af175a6609a5092e7
ibrahimkhidr <akhidrb@gmail.com>
2026-01-30 12:21:10 -0800

    add Frontend

--- app/api.py
+++ app/api.py
@@ -1,8 +1,16 @@
+from pathlib import Path
+from fastapi.responses import FileResponse
 from fastapi import APIRouter, UploadFile, File
 from app.controllers.qa_controller import handle_qa
 from app.schemas import QAResponse
 
 router = APIRouter()
+
+
+@router.get("/", include_in_schema=False)
+def index():
+    path = Path(__file__).resolve().parent / "static" / "index.html"
+    return FileResponse(path)
 
 
 @router.post("/qa", response_model=QAResponse)
